ef5015e0337155ae80cf5d1701b7344fc3aea2eb
Keir Finlow-Bates <keir@chainfrog.com>
2019-08-09 16:43:50 +0300

    Added product name and stuff

--- amazon-bestseller.py
+++ amazon-bestseller.py
@@ -11,17 +11,27 @@ driver = webdriver.Chrome(home + '/.local/bin/chromedriver')
 sites = ['com', 'co.uk', 'de', 'fr', 'es', 'com.mx', 'ca', 'co.jp', 'com.br', 'com.au', 'in' ]
 
 html="<html><head></head><body><h1>" + sys.argv[1] + "</h1>"
+failure = ""
+success = True
+first = True
 
 for locale in sites:
     driver.get('https://www.amazon.' + locale + '/dp/' + sys.argv[1])
+    if first:
+        first = False
+        html = html + "<p><b>" + driver.title + "</b></p>"
     try:
         rank_element = driver.find_element_by_css_selector('li[id="SalesRank"]')
         rank = rank_element.get_attribute('innerHTML')
+        html = html + "<h2>" + locale + "</h2>"
+        html = html + rank
     except:
         rank = "<p>No rank found</p>"
-    html = html + "<h2>" + locale + "</h2>"
-    html = html + rank
+        success = False
+        failure = failure + "<li>" + locale + "</li>"
 
+if not success:
+    html = html + "<h2> No results for:</h2><ul>" + failure + "</ul>"
 
 html = html + "</body></html>"
 driver.execute_script("document.write('{}')".format(json.dumps(html)))
